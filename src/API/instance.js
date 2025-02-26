71bb2e34cd7e6a9f3eedff952ae286fc82259c32
Mahmoud Elzaw <hoodaelzaw81@gmail.com>
2025-02-26 09:10:35 +0200

    instance update

--- src/API/instance.js
+++ src/API/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://running-antelope-elzaw-25aed18b.koyeb.app/api",
 });
 
 export default instance;
